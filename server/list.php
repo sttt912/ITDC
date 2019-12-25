89bb5fa2cd606f18eeef3a7c2c6504817bd9bdb5
sttt912 <sttt912@yahoo.com>
2019-12-26 00:42:25 +0200

    v0.11
    v0.11

--- server/list.php
+++ server/list.php
@@ -1,48 +1,13 @@
 <?  
 include("inc/conect.php");
 include("inc/head.php");
-?>
-
-  <?php
- 
-    if (isset($_POST["numbers"])) {
-      if (isset($_GET['red'])) {
-        $sql = mysqli_query($link, "UPDATE `transport` SET `numbers` = '{$_POST['numbers']}',`driver` = '{$_POST['driver']}',`spu` = '{$_POST['spu']}',`status` = '{$_POST['status']}' WHERE `ID`={$_GET['red']}");
-      } else {
-        $sql = mysqli_query($link, "INSERT INTO `transport` (`numbers`, `driver`, `spu`, `status`) VALUES ('{$_POST['numbers']}', '{$_POST['driver']}', '{$_POST['spu']}', '{$_POST['status']}')");
-      }
-
-    if ($sql) {
-        echo '<p>Успешно!</p>';
-      } else {
-        echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
-      }
-    }
-
-    if (isset($_GET['del'])) {
-      $sql = mysqli_query($link, "DELETE FROM `transport` WHERE `ID` = {$_GET['del']}");
-      if ($sql) {
-        echo "<p>удален.</p>";
-      } else {
-        echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
-      }
-    }
-
-    if (isset($_GET['red'])) {
-      $sql = mysqli_query($link, "SELECT `ID`, `numbers`, `driver`, `spu`, `status` FROM `transport` WHERE `ID`={$_GET['red']}");
-      $product = mysqli_fetch_array($sql);
-	  include("inc/form.php");
-    }
-  ?>
+include("inc/func.php"); 
   
-  <?php
   $sql = mysqli_query($link, 'SELECT `ID`, `numbers`, `driver`, `spu`, `status` FROM `transport`');
+  
   while ($result = mysqli_fetch_array($sql)) {
     echo "<p>{$result['ID']})||{$result['numbers']}||{$result['driver']}||{$result['spu']}||{$result['status']}- <a href='?del={$result['ID']}'>Удалить</a> ||<a href='?red={$result['ID']}'>Редактировать</a></p>";
   }
-  ?>
-  
-  
-<?  
+
 include("inc/foot.php");
 ?>
